f9831a805048d0428a18e480673f50c7be992cd8
Mateusz Paluszkiewicz <150561295+MatPaluch@users.noreply.github.com>
2024-08-16 15:41:00 +0000

    Mobile_is_done

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,16 +3,22 @@ import styles from "./Nav.module.css";
 import { useMediaQuery } from "@mui/material";
 import { ReactSVG } from "react-svg";
 import menu from "../../assets/menu3.svg";
+import { useState } from "react";
 
 export const Navigation = () => {
-  const isSmallScreen = useMediaQuery("(max-width:1024px)");
+  const [isActive, setIsActive] = useState(false);
+  const isSmallScreen = useMediaQuery("(max-width:960px)");
+
+  const mobileMenuHandler = (ev) => {
+    setIsActive(!isActive);
+  };
   return (
     <nav className={styles.navBar}>
       <NavLink to="/">
         <div className={styles.logo}></div>
       </NavLink>
       {isSmallScreen ? (
-        <button className={styles.menuButton}>
+        <button className={styles.menuButton} onClick={mobileMenuHandler}>
           <ReactSVG
             src={menu}
             beforeInjection={(svg) => {
@@ -39,25 +45,47 @@ export const Navigation = () => {
           </li>
         </ul>
       )}
-      <div>
-        <ul>
-          <li>
-            <NavLink to="news">Aktualności</NavLink>
-          </li>
-          <li>
-            <NavLink to="about">O nas</NavLink>
-          </li>
-          <li>
-            <NavLink to="projects">Nasze projekty</NavLink>
-          </li>
-          <li>
-            <NavLink to="statute">Statut</NavLink>
-          </li>
-          <li>
-            <NavLink to="contact">Kontakt</NavLink>
-          </li>
-        </ul>
-      </div>
+      {isActive && isSmallScreen && (
+        <div className={styles.mobileNav}>
+          <div className={styles.divButton}>
+            <button className={styles.mobileButton} onClick={mobileMenuHandler}>
+              <ReactSVG
+                src={menu}
+                beforeInjection={(svg) => {
+                  svg.setAttribute("fill", "white");
+                }}
+              />
+            </button>
+          </div>
+          <ul className={styles.mobileList}>
+            <li>
+              <NavLink to="news" onClick={mobileMenuHandler}>
+                Aktualności
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="about" onClick={mobileMenuHandler}>
+                O nas
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="projects" onClick={mobileMenuHandler}>
+                Nasze projekty
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="statute" onClick={mobileMenuHandler}>
+                Statut
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="contact" onClick={mobileMenuHandler}>
+                Kontakt
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 };
